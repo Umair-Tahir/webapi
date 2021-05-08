57fc8497b7e0d4e23fece19ac0bc0559884b77db
Haris Ch <hqayyum47@gmail.com>
2021-05-08 13:54:20 +0500

    conflict resolved -latest order APIs

--- app/Http/Controllers/API/GenerateOrderAPIController.php
+++ app/Http/Controllers/API/GenerateOrderAPIController.php
@@ -57,11 +57,11 @@ class GenerateOrderAPIController extends Controller
     public function pickupOrder(CreateOrderPickUpRequest $request)
     {
         try {
-            $input = $request->all();
-            $input['delivery_type_id'] = 1;
-            $input['delivery_address_id'] = null;
-            $input['delivery_fee'] = null;
-            $input ['tip'] = null;
+        $input = $request->all();
+        $input['delivery_type_id'] = 1;
+        $input['delivery_address_id'] = null;
+        $input['delivery_fee'] = null;
+        $input ['tip'] = null;
 
 
             /******  Find User ******/
@@ -126,6 +126,7 @@ class GenerateOrderAPIController extends Controller
     }
 
     /*************Restaurant Delivery order request *************/
+
     public function restaurantDeliveryOrder(CreateOrderRestaurantDeliveryRequest $request){
 
         try {
@@ -199,8 +200,8 @@ class GenerateOrderAPIController extends Controller
     public function deliveryServiceOrder(CreateOrderEvaDeliveryRequest $request)
     {
         try {
-            $input = $request->all();
-            $input['delivery_type_id'] = 3;
+        $input = $request->all();
+        $input['delivery_type_id'] = 3;
 
             /******  Find User ******/
             $user = $this->userRepository->findWithoutFail($input['user_id']);
