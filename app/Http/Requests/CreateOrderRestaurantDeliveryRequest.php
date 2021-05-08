1fc450ebef3aa4defc524e2faa9236bfffda1d61
Haris Ch <hqayyum47@gmail.com>
2021-05-08 13:51:14 +0500

    delivery type ID removed from res delivery req

--- app/Http/Requests/CreateOrderRestaurantDeliveryRequest.php
+++ app/Http/Requests/CreateOrderRestaurantDeliveryRequest.php
@@ -31,7 +31,6 @@ class CreateOrderRestaurantDeliveryRequest extends FormRequest
             'expiry_year'   => 'required',
             'cvc_code'   => 'required',
             "user_id"       => 'required',
-            "delivery_type_id"       => 'required',
             "delivery_address_id" => 'required',
             "delivery_fee"        => 'required',
             'is_french'           => 'required',
